1da9215c2dd39d2c9becc2221fe492c31378a45b
Ryan Atkinson <mail@ryanatkn.com>
2023-07-17 07:25:09 -0600

    more

--- src/routes/pointing-events/+page.svelte
+++ src/routes/pointing-events/+page.svelte
@@ -13,6 +13,7 @@
 		items.unshift({name, time: performance.now()});
 	};
 
+	// TODO BLOCK add mousemove, but only between a mousedown and mouseup to avoid worthless spam
 	const mouse =
 		(name: string) =>
 		(e: MouseEvent): void => {
@@ -20,20 +21,31 @@
 			console.log(`e`, e);
 		};
 
-	$: start_time = items[items.length - 1]?.time;
+	$: item_count = items.length;
+	$: start_time = items[item_count - 1]?.time;
 </script>
 
 <div class="wrapper">
 	<div class="content">
 		<ul class="log">
-			{#each items as item (item)}
+			{#each items as item, i (item)}
+				{@const time = item.time - start_time}
+				{@const dt = i === item_count - 1 ? time : time - (items[i + 1].time - start_time)}
 				<li>
 					<div class="name">{item.name}</div>
-					<div>{Math.round(item.time - start_time)}<small>ms</small></div>
+					<div class="dt">{Math.round(dt)}<small>ms</small></div>
+					<div class="time">{Math.round(time)}<small>ms</small></div>
 				</li>
 			{/each}
 		</ul>
-		<div class="pointing_events" role="none" on:click={mouse('click')} />
+		<div
+			class="pointing_events"
+			role="none"
+			on:click={mouse('click')}
+			on:dblclick={mouse('dblclick')}
+			on:mousedown={mouse('mousedown')}
+			on:mouseup={mouse('mouseup')}
+		/>
 	</div>
 	<div class="scrollable">
 		<button on:click={() => (items = [])}>clear</button>
@@ -68,15 +80,23 @@
 		left: 0;
 		top: 0;
 		z-index: 0;
+		text-align: right;
 	}
 	li {
 		display: flex;
 		align-items: center;
 	}
 	.name {
-		width: 80px;
+		width: 120px;
 	}
 	small {
 		color: var(--text_3);
+	}
+	.time {
+		width: 80px;
+	}
+	.dt {
+		width: 70px;
+		text-align: right;
 	}
 </style>
