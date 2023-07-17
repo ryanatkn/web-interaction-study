78d3e229ac460592840137a1279357274bb14195
Ryan Atkinson <mail@ryanatkn.com>
2023-07-17 09:35:12 -0600

    deployment

--- _app/immutable/chunks/stores.8100bb97.js
+++
@@ -1,1 +0,0 @@
-import"./boolean_attributes.39e06bd0.js";import{d as e}from"./singletons.4ad94f75.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
