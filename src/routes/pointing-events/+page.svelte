1a30a5b00da805afc3153ffa65fb484ad251d6c3
Ryan Atkinson <mail@ryanatkn.com>
2023-07-17 06:59:14 -0600

    add pointing events study

--- src/routes/pointing-events/+page.svelte
+++ src/routes/pointing-events/+page.svelte
@@ -1,5 +1,48 @@
 <script lang="ts">
-	//
+	// TODO BLOCK capture (toggle?)
+
+	interface LogItem {
+		name: string;
+		time: number;
+	}
+
+	let items: LogItem[] = [];
+
+	const log = (name: string) => {
+		items = items.slice();
+		items.unshift({name, time: performance.now()});
+	};
+
+	const mouse =
+		(name: string) =>
+		(e: MouseEvent): void => {
+			log(name);
+			console.log(`e`, e);
+		};
+
+	$: start_time = items[items.length - 1]?.time;
 </script>
 
-wip
+<div class="pointing_events" role="none" on:click={mouse('click')}>
+	<ul class="log">
+		{#each items as item (item)}
+			<li>
+				{item.name} -
+				<div>{Math.round(item.time - start_time)}<small>ms</small></div>
+			</li>
+		{/each}
+	</ul>
+</div>
+
+<style>
+	.pointing_events {
+		position: fixed;
+		inset: 0;
+	}
+	.log {
+		z-index: 0;
+	}
+	small {
+		color: var(--text_3);
+	}
+</style>
