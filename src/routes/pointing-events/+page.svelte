c2df8a4393f8c95299fec3d787b5310f96a6275e
Ryan Atkinson <mail@ryanatkn.com>
2023-07-17 07:11:58 -0600

    layout

--- src/routes/pointing-events/+page.svelte
+++ src/routes/pointing-events/+page.svelte
@@ -23,24 +23,58 @@
 	$: start_time = items[items.length - 1]?.time;
 </script>
 
-<div class="pointing_events" role="none" on:click={mouse('click')}>
-	<ul class="log">
-		{#each items as item (item)}
-			<li>
-				{item.name} -
-				<div>{Math.round(item.time - start_time)}<small>ms</small></div>
-			</li>
-		{/each}
-	</ul>
+<div class="wrapper">
+	<div class="content">
+		<ul class="log">
+			{#each items as item (item)}
+				<li>
+					<div class="name">{item.name}</div>
+					<div>{Math.round(item.time - start_time)}<small>ms</small></div>
+				</li>
+			{/each}
+		</ul>
+		<div class="pointing_events" role="none" on:click={mouse('click')} />
+	</div>
+	<div class="scrollable">
+		<button on:click={() => (items = [])}>clear</button>
+	</div>
 </div>
 
 <style>
-	.pointing_events {
+	.wrapper {
 		position: fixed;
 		inset: 0;
+		display: flex;
+	}
+	.content {
+		position: relative;
+		flex: 1;
+		height: 100%;
+	}
+	.pointing_events {
+		position: absolute;
+		inset: 0;
+	}
+	.scrollable {
+		width: 80px;
+		height: 100%;
+		background-color: var(--fg_1);
+		display: flex;
+		flex-direction: column;
+		align-items: stretch;
 	}
 	.log {
+		position: absolute;
+		left: 0;
+		top: 0;
 		z-index: 0;
+	}
+	li {
+		display: flex;
+		align-items: center;
+	}
+	.name {
+		width: 80px;
 	}
 	small {
 		color: var(--text_3);
